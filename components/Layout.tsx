7f0bc272b2f09c4462f089eb90e18e3085d450dc
SynCROSS <kuuhaku3125@gmail.com>
2021-03-30 17:13:54 +0900

    Check Logged In

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
+import { checkLoggedInOrLogin } from '../containers/auth/LoginContainer';
 import Footer from './common/Footer';
 import Header from './common/Header';
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    if (!(checkLoggedInOrLogin('', '') ?? false)) {
+      if (
+        location.href !== 'http://localhost:3000/' &&
+        location.href !== 'http://localhost:3000/Register'
+      ) {
+        alert('You Should Log In!');
+        location.href = 'http://localhost:3000/';
+      }
+    } else {
+      if (location.href === 'http://localhost:3000/')
+        location.href = 'http://localhost:3000/Home';
+    }
+  }, [children]);
   return (
     <div
       className="flex jc-center ai-center flex-dir-col"
--- containers/auth/LoginContainer.tsx
+++ containers/auth/LoginContainer.tsx
@@ -1,26 +1,43 @@
 import axios from 'axios';
 import Login from '../../components/auth/Login';
 
-const LoginContainer = () => {
-  const login = async (username: string, password: string) => {
-    try {
+export const checkLoggedInOrLogin = async (
+  username: string,
+  password: string,
+) => {
+  try {
+    const cookie = document.cookie.split('=');
+    if (document.cookie && cookie[0] === 'Authentication') {
+      const loggedIn = await axios.get('http://localhost:4000/auth/check', {
+        headers: {
+          Authorization: cookie[1],
+        },
+      });
+
+      return loggedIn?.data;
+    } else if (!document.cookie) {
       const response = await axios.post('http://localhost:4000/auth/login', {
         username,
         password,
       });
+
       if (response.status < 200 && response.status >= 300) {
         return null;
       }
-      document.cookie = `Authentication=${response.data}; Max-Age=${
-        60 * 60 * 1000 * 24 * 2
-      }`;
-      console.log(JSON.parse(response.config.data).username);
-    } catch (e) {
-      console.error(e);
-    }
-  };
 
-  return <Login login={login} />;
+      document.cookie = `Authentication=${response.data}; Max-Age=${
+        60 * 60 * 24 * 2
+      }`;
+
+      location.href;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const LoginContainer = () => {
+  return <Login login={checkLoggedInOrLogin} />;
 };
 
 export default LoginContainer;
