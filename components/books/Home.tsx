9e6130e4110a3afcf31e435ed0b93b3b1cecf56f
SynCROSS <kuuhaku3125@gmail.com>
2021-03-31 17:10:40 +0900

    Add Home Page

--- components/books/Home.tsx
+++ components/books/Home.tsx
@@ -1,8 +1,28 @@
+import Link from 'next/link';
 import { useState } from 'react';
 import styled from 'styled-components';
 
 const HomeBlock = styled.div`
   margin: auto;
+  width: 50%;
+`;
+
+const BookList = styled.ul`
+  flex-wrap: wrap;
+  align-items: baseline;
+`;
+
+const BookItem = styled.li`
+  margin: 0.5rem 1rem;
+  width: 180px;
+`;
+
+const BookImage = styled.img`
+  width: 100px;
+  height: 150px;
+  border-radius: 5px;
+  background-color: #fefefe;
+  box-shadow: 10px 10px 30px #d8d8d8, -10px -10px 30px #ffffff;
 `;
 
 const Home = ({ books }) => {
@@ -10,19 +30,28 @@ const Home = ({ books }) => {
 
   books.then(result => setData(result.data));
 
-  console.log(data);
-
   return (
     <HomeBlock>
-      <ul>
+      <BookList className="flex jc-center ai-center">
         {data?.map(book => (
-          <li key={book.book_id}>
-            <img width="100" src={book.cover} alt={book.title} />
-            <br />
-            {book.title}
-          </li>
+          <BookItem key={book.book_id}>
+            <Link href={`/${book.book_id}`}>
+              <a>
+                <BookImage
+                  src={book.cover}
+                  alt={book.title}
+                  placeholder=""
+                  loading="lazy"
+                />
+                <br />
+                {book.title.length > 30
+                  ? book.title.slice(0, 30) + ' . . . '
+                  : book.title}
+              </a>
+            </Link>
+          </BookItem>
         ))}
-      </ul>
+      </BookList>
     </HomeBlock>
   );
 };
