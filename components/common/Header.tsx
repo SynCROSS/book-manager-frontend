d89fe65abd05765b72459a59333d07f6bb7c5c8b
SynCROSS <kuuhaku3125@gmail.com>
2021-03-31 17:10:58 +0900

    Update Header.tsx

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 const HeaderBlock = styled.header`
   width: 100%;
   padding: 1rem 0;
-  position: fixed;
+  position: sticky;
   top: 0;
   left: 0;
 
-  background-color: rgba(255, 255, 255, 0.1);
+  background-color: rgba(255, 255, 255, 0.9);
   /* background: transparent; */
   box-shadow: 0 0 2px 2px #ccc;
 `;
