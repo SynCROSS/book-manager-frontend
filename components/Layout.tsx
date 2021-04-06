01a8c2f7e16a4a98c8a52e5333be415e581ebc53
SynCROSS <kuuhaku3125@gmail.com>
2021-04-06 18:56:15 +0900

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,24 +3,25 @@ import { checkLoggedInOrLogin } from '../containers/auth/LoginContainer';
 import Footer from './common/Footer';
 import Header from './common/Header';
 
+const checkLoggedIn = async () => {
+  const response = await checkLoggedInOrLogin('', '');
+
+  if (!(response ?? false)) {
+    if (
+      location.href !== 'http://localhost:3000/' &&
+      location.href !== 'http://localhost:3000/Register'
+    ) {
+      location.href = 'http://localhost:3000/';
+    }
+  } else {
+    if (location.href === 'http://localhost:3000/') {
+      location.href = 'http://localhost:3000/Home';
+    }
+  }
+};
+
 const Layout = ({ children }) => {
   useEffect(() => {
-    const checkLoggedIn = async () => {
-      const response = await checkLoggedInOrLogin('', '');
-
-      if (!(response ?? false)) {
-        if (
-          location.href !== 'http://localhost:3000/' &&
-          location.href !== 'http://localhost:3000/Register'
-        ) {
-          location.href = 'http://localhost:3000/';
-        }
-      } else {
-        if (location.href === 'http://localhost:3000/') {
-          location.href = 'http://localhost:3000/Home';
-        }
-      }
-    };
     checkLoggedIn();
   }, [children]);
   return (
